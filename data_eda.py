32bf9b08bafc2ec50328b9e6f68a421b9762e870
solisent <sentaddis@gmail.com>
2023-01-26 10:58:14 +0800

    EDA code added

---
+++ data_eda.py
@@ -0,0 +1,152 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+df = pd.read_csv('salary_data_cleaned.csv')
+df.head()
+
+def title_simplifier(title):
+    if 'data scientist' in title.lower():
+        return 'data scientist'
+    elif 'data engineer' in title.lower():
+        return 'data engineer'
+    elif 'analyst' in title.lower():
+        return 'analyst'
+    elif 'machine learning' in title.lower():
+        return 'mle'
+    elif 'manager' in title.lower():
+        return 'manager'
+    elif 'director' in title.lower():
+        return 'director'
+    else:
+        return 'na'
+    
+def seniority(title):
+    if 'sr' in title.lower() or 'senior' in title.lower() or 'sr' in title.lower() or 'lead' in title.lower() or 'principal' in title.lower():
+            return 'senior'
+    elif 'jr' in title.lower() or 'jr.' in title.lower():
+        return 'jr'
+    else:
+        return 'na'
+# as part of EDA add more data cleaning and more feature engineering such as the following		
+## Job title and seniority 		
+##  Fix state Los Angeles 
+##  Job description length 
+##  Competitor count
+## hourly wage to annual 
+#remove new line from job title
+
+df['job_simp'] = df['Job Title'].apply(title_simplifier)
+df.job_simp.value_counts()
+df['seniority'] = df['Job Title'].apply(seniority)
+df.seniority.value_counts()
+
+# Fix state Los Angeles 
+df['job_state']= df.job_state.apply(lambda x: x.strip() if x.strip().lower() != 'los angeles' else 'CA')
+df.job_state.value_counts()
+
+#  Job description length 
+df['desc_len'] = df['Job Description'].apply(lambda x: len(x))
+df['desc_len']
+
+#Competitor count
+df['num_comp'] = df['Competitors'].apply(lambda x: len(x.split(',')) if x != '-1' else 0)
+df['Competitors']
+
+#hourly wage to annual 
+df['min_salary'] = df.apply(lambda x: x.min_salary*2 if x.hourly ==1 else x.min_salary, axis =1)
+df['max_salary'] = df.apply(lambda x: x.max_salary*2 if x.hourly ==1 else x.max_salary, axis =1)
+df[df.hourly ==1][['hourly','min_salary','max_salary']]
+
+#remove new line character from company names
+df['company_txt'] = df.company_txt.apply(lambda x: x.replace('\n', ''))
+df['company_txt']
+
+#EDA starts here
+df.describe()
+# histograms with nearly normal => suitable for most analysis, if skewed normalization of that feature may be required
+df.columns
+
+#examine continuous variables
+df.Rating.hist()
+df.avg_salary.hist()
+df.age.hist() 
+df.desc_len.hist()
+
+#box plots with different scale indicates the requirement of normalization
+df.boxplot(column = ['age','avg_salary','Rating'])
+df.boxplot(column = 'Rating')
+
+# for model building we need to know what is highly correlated with what
+# we also want to avoid multicolinearity if we want to use regression
+df[['age','avg_salary','Rating','desc_len']].corr()
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(df[['age','avg_salary','Rating','desc_len','num_comp']].corr(),vmax=.3, center=0, cmap=cmap,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+#examine the categorical variables (bar charts to examine quantities)
+df_cat = df[['Location', 'Headquarters', 'Size','Type of ownership', 'Industry', 'Sector', 'Revenue', 'company_txt', 'job_state','same_state', 'python_yn', 'R_yn',
+       'spark', 'aws', 'excel', 'job_simp', 'seniority']]
+
+for i in df_cat.columns:
+    cat_num = df_cat[i].value_counts()
+    print("graph for %s: total = %d" % (i, len(cat_num)))
+    chart = sns.barplot(x=cat_num.index, y=cat_num)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
+    plt.show()
+
+#filtering for better visibility
+for i in df_cat[['Location','Headquarters','company_txt']].columns:
+    cat_num = df_cat[i].value_counts()[:20]
+    print("graph for %s: total = %d" % (i, len(cat_num)))
+    chart = sns.barplot(x=cat_num.index, y=cat_num)
+    chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
+    plt.show()
+
+#pivot tables to see quantities with certain categorization (grouping)
+pd.pivot_table(df, index = 'job_simp', values = 'avg_salary')
+pd.pivot_table(df, index = ['job_simp','seniority'], values = 'avg_salary')
+pd.pivot_table(df, index = ['job_state','job_simp'], values = 'avg_salary').sort_values('job_state', ascending = False)
+
+pd.options.display.max_rows
+pd.set_option('display.max_rows', None)
+pd.pivot_table(df, index = ['job_state','job_simp'], values = 'avg_salary', aggfunc = 'count').sort_values('job_state', ascending = False)
+pd.pivot_table(df[df.job_simp == 'data scientist'], index = 'job_state', values = 'avg_salary').sort_values('avg_salary', ascending = False)
+
+df_pivots = df[['Rating', 'Industry', 'Sector', 'Revenue', 'num_comp', 'hourly', 'employer_provided', 'python_yn', 'R_yn', 'spark', 'aws', 'excel', 'Type of ownership','avg_salary']]
+for i in df_pivots.columns:
+    print(i)
+    print(pd.pivot_table(df_pivots,index =i, values = 'avg_salary').sort_values('avg_salary', ascending = False))
+
+pd.pivot_table(df_pivots, index = 'Revenue', columns = 'python_yn', values = 'avg_salary', aggfunc = 'count')
+
+#generating wordcloud
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+words = " ".join(df['Job Description'])
+
+def punctuation_stop(text):
+    """remove punctuation and stop words"""
+    filtered = []
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(text)
+    for w in word_tokens:
+        if w not in stop_words and w.isalpha():
+            filtered.append(w.lower())
+    return filtered
+
+
+words_filtered = punctuation_stop(words)
+
+text = " ".join([ele for ele in words_filtered])
+
+wc= WordCloud(background_color="white", random_state=1,stopwords=STOPWORDS, max_words = 2000, width =800, height = 1500)
+wc.generate(text)
+
+plt.figure(figsize=[10,10])
+plt.imshow(interpolation="bilinear")
+plt.axis('off')
+plt.show()
+
